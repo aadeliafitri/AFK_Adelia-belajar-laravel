40693ad50990861c4f741c530a016adc964015e0
aadeliafitri <adelia_fitri_27rpl@student.smktelkom-mlg.sch.id>
2023-11-03 11:13:54 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <h1 class="m-3">Hello World</h1>
+    <div class="card m-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="{{ asset('image/profil-adel.jpg') }}" class="img-fluid rounded-start" alt="..." width="150px">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Adelia Fitri Kristanti</h5>
+              <p class="card-text">Studi Independen Bersertifikat</p>
+              <p class="card-text"><small class="text-body-secondary">Full Stack Web Developer</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    <script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </script>
+</body>
+</html>
